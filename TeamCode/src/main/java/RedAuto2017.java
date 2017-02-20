37dbe17367262cf4248234c305ecfd5a2d98d9f8
G-BOTS <ferasmus@wlcalv.com>
2017-02-19 23:35:09 -0800

    add some of the user defined functions up to 24min into the youtube video

--- TeamCode/src/main/java/RedAuto2017.java
+++ TeamCode/src/main/java/RedAuto2017.java
@@ -32,97 +32,219 @@ public class RedAuto2017 {
 }
 //WaitForStart*/
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-Public class experstatemachine extends OpMode
+import static android.R.attr.mode;
+
+public class experstatemachine extends OpMode
 {
-    //a list of all the system states
-    private enum State {
-        STATE_INITIAL,
-        STATE_DRIVE_TO_SHOOT,
-        STATE_RELOAD_NEXT_SHOT,
-        STATE_STOP,
-                _}
-//pathseg are defined as distances for left wheel and right wheel and then the power to the motors
-    final PathSeg[] mshootpospath ={
-            new PathSeg( 3.2, 3.2, 0.6), //forward
-            new PathSeg( -4.0, 4.0, 0.4) //slowdown and rotate  left
-    };
+        //a list of all the system states
+        private enum State {
+            STATE_INITIAL,
+            STATE_DRIVE_TO_SHOOT,
+            STATE_RELOAD_NEXT_SHOT,
+            STATE_STOP,
+                    _}
+    //pathseg are defined as distances for left wheel and right wheel and then the power to the motors
+        final PathSeg[] mshootpospath ={
+                new PathSeg( 3.2, 3.2, 0.6), //forward
+                new PathSeg( -4.0, 4.0, 0.4) //slowdown and rotate  left
+        };
 
-    final PathSeg[] mParalleltowallpath ={
-            new PathSeg( 3.2, 3.2, 0.6), //forward
-            new PathSeg( -4.0, 4.0, 0.4) //slowdown and rotate  left
-    };
-
-
-    final double TicksPerInch =240;
-
-    final double TriggerForShoot=500;
-    final double ShooterFullExtend=500;
-
-    //robot devices
-    public DcMotor leftMotor = null;
-    public DcMotor rightMotor = null;
-    public DcMotor Elevator = null;
-    public DcMotor Trigger = null;
-    public DcMotor Lift = null;
-    public DcMotor Intake = null;
-    public Servo rightClaw = null;
-    public GyroSensor Gyro = null;
-
-    private int mLeftEncoder;
-    private int mRightEncoder;
-    //
-    //loop cycle time stats variables
-    Public ElapsedTime  mRunTime = new ELapsedTime();  //time into round
-
-    private ElapsedTime mStateTime = new ElapsedTime():  //time into state
-
-    private state  mcurrentState; //statemachine state
-    private PathSeg mcurrentPath; //array to hold path
-    private int     mcurrentSeg; //Index of the current seg in the current path
-
-    //init
-
-@Overide
-    public void init()
-{
-    //initialise class members
-
-    leftMotor   = hwMap.dcMotor.get("left_drive");
-    rightMotor  = hwMap.dcMotor.get("right_drive");
-    Elevator = hwMap.dcMotor.get("Elevator");
-    Trigger = hwMap.dcMotor.get("Trigger");
-    Lift = hwMap.dcMotor.get("Lift");
-    Intake = hwMap.dcMotor.get("Intake");
-    leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-    rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-    Elevator.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-    Trigger.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-    Lift.setDirection(DcMotor.Direction.FORWARD);
-    Intake.setDirection(DcMotor.Direction.FORWARD);
-
-    setDrivePower(0,0); //make sur the robot is standing still
-    resetDriveEncoders();
+        final PathSeg[] mParalleltowallpath ={
+                new PathSeg( 3.2, 3.2, 0.6), //forward
+                new PathSeg( -4.0, 4.0, 0.4) //slowdown and rotate  left
+        };
 
 
-}
+        final double TicksPerInch =240;
 
-//loop
+        final double TriggerForShoot=500;
+        final double ShooterFullExtend=500;
+
+        //robot devices
+        public DcMotor leftMotor = null;
+        public DcMotor rightMotor = null;
+        public DcMotor Elevator = null;
+        public DcMotor Trigger = null;
+        public DcMotor Lift = null;
+        public DcMotor Intake = null;
+        public Servo rightClaw = null;
+        public GyroSensor Gyro = null;
+
+        private int mLeftEncoder;
+        private int mRightEncoder;
+        //
+        //loop cycle time stats variables
+        public ElapsedTime mRunTime = new ElapsedTime();  //time into round
+
+        private ElapsedTime mStateTime = new ElapsedTime();  //time into state
+
+        private STATE  mcurrentState; //statemachine state
+        private PathSeg mcurrentPath; //array to hold path
+        private int     mcurrentSeg; //Index of the current seg in the current path
+
+        //init
+
     @Override
-    public void init_loop()
+        public void init()
     {
+        //initialise class members
+
+        leftMotor   = HardwareMap.dcMotor.get("left_drive");
+        rightMotor  = hwMap.dcMotor.get("right_drive");
+        Elevator = hwMap.dcMotor.get("Elevator");
+        Trigger = hwMap.dcMotor.get("Trigger");
+        Lift = hwMap.dcMotor.get("Lift");
+        Intake = hwMap.dcMotor.get("Intake");
+        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        Elevator.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        Trigger.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        Lift.setDirection(DcMotor.Direction.FORWARD);
+        Intake.setDirection(DcMotor.Direction.FORWARD);
+
+        setDrivePower(0,0); //make sure the robot is standing still
         resetDriveEncoders();
-        telemetry.addData("ENC",String.format("L:R %d:%d",getLeftPossition(), getRightPossition()));
+
+
+    }
+
+    //loop
+    @Override
+    public void init_loop() {
+        resetDriveEncoders();
+        telemetry.addData("ENC", String.format("L:R %d:%d", getLeftPossition(), getRightPossition()));
+        switch (McurrentState)
+        {
+            case STATE_INITIAL:// stay in this state until both encoders are zero
+                if (encoders AtZero())
+                {
+                    StatePath(mshootpospath);  // Load path to shooting posisition
+                    newState(STATE_DRIVE_TO_SHOOT);
+                }
+
+                else
+
+                {
+                    // display diagnostic data for this sate{}
+                    telemetry.addData("1", String.format("L:R %7f:%7f", getLeftPossition(), getRightPossition()));
+
+                }
+
+            break;
+
+            case STATE_DRIVE_TO_SHOOT:// stay in this state until both encoders are zero
+                if (encoders AtZero())
+                {
+                    StatePath(mshootpospath);  // Load path to shooting posisition
+                    newState(STATE_DRIVE_TO_SHOOT);
+                }
+                else
+                {
+                    // display diagnostic data for this sate{}
+                    telemetry.addData("1", String.format("L:R %7f:%7f", getLeftPossition(), getRightPossition()));
+
+                }
+
+            break;
+
+
+            case STATE_RELOAD_NEXT_SHOT:// stay in this state until both encoders are zero
+                if (encoders AtZero())
+                {
+                    StatePath(mshootpospath);  // Load path to shooting posisition
+                    newState(STATE_DRIVE_TO_SHOOT);
+                }
+
+                else
+                {
+                    // display diagnostic data for this sate{}
+                    telemetry.addData("1", String.format("L:R %7f:%7f", getLeftPossition(), getRightPossition()));
+
+                }
+
+            break;
+            case STATE_STOP:
+            break;
+        }
+    }
+    @Override
+    public void stop()
+    {
+     //ensure that the motors are turned off
+        useConstantPower();
+        setDrivePower(0, 0);
+    }
+    //user defined utility functions here
+
+    //transition to a new state
+    private void newState(State newState)
+    {//reset state time
+        mStateTime.reset();
+        mcurrentState = newState;
+    }
+    void setEncoderTarget( int LeftEncoder, int RightEncoder)
+    {
+        leftMotor.setTargetPosition(mLeftEncoderTarget = LeftEncoder);
+        rightMotor.setTargetPosition(mRightEncoderTarget = RightEncoder);
+
+    }
+    void addEncoderTarget( int LeftEncoder, int RightEncoder)
+    {
+        leftMotor.setTargetPosition(mLeftEncoderTarget  += LeftEncoder);
+        rightMotor.setTargetPosition(mRightEncoderTarget += RightEncoder);
+
+    }
+    void setDrivePower(double leftPower, double rightPower)
+    {
+      leftMotor.setPower(Range.clip(leftPower, -1,1));
+        rightMotor.setPower(Range.clip(rightPower,-1,1));
+    }
+
+    void setDriveSpeed(double leftSpeed, double rightSpeed)
+    {
+        setDrivePower(leftSpeed, rightSpeed);
+        }
+    public void runToPosition()
+    {
+        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
 
+    public  void useConstantSpeed()
+    {
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
 
-    fina
+    public  void useConstantPower()
+    {
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
 
 
-
-
+    public void resetDriveEncoders()
+    {
+    setEncoderTarget(0, 0);
+        setMode(DcMotor.RESET_ENCODERS);
+        }
+    public void synchEncoders()
+    {
+        mLeftEncoderTarget = leftMotor.getCurrentPosition();
+        mRightEncoderTarget = rightMotor.getCurrentPosition();
+    }
+    public void setDriveMode(DcMotor.RunMode mode)
+    {
+        if (leftMotor.getChannelMode()!= mode)
+        {
+            leftMotor.setChannelMode(mode);
+        }
+    }
 }
