0aa15e3a899d8d1350c17d8d8f3be10205d06721
ferasmus <ferasmus@wlcalv.com>
2017-04-25 09:19:17 -0700

    Demo teleop class

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/Demo.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import android.os.SystemClock;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.Timer;
+
+/**
+ * Created by ferasmus on 4/24/2017.
+ */
+
+public class Demo extends OpMode
+{
+    DcMotor leftmotor;
+    DcMotor rightmotor;
+
+    HardwareMap hwMap = null;
+
+    @Override
+    public void init(){
+
+        leftmotor  =hwMap.dcMotor.get("left_drive");
+        rightmotor =hwMap.dcMotor.get("right_drive");
+
+        //reverse the right motor
+        rightmotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void start ()
+    {
+        resetStartTime();
+    }
+
+    @Override
+    public void loop ()
+    {
+        while(getRuntime() < 30)
+        {
+            float leftY = -gamepad1.left_stick_y;
+
+            float rightY = -gamepad1.right_stick_y;
+
+            leftmotor.setPower(leftY);
+            rightmotor.setPower(rightY);
+        }
+        if((gamepad1.a) && (gamepad1.b))
+        {
+            resetStartTime();
+        }
+    }
+}
